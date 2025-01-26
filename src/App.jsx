69019934503951dda28e60308f30992e6ed706b1
Vismita <167453494+Vismita099@users.noreply.github.com>
2025-01-26 15:50:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Footer from "./components/Footer/Footer";
-import Project from "./components/Project/project";
+import Project from "./components/Project/Projects";
 
 function App() {
   return (
